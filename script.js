0f5cde7b149dfce32e18fc90154c5ee5d9b4fbd0
w41w41-beep <satoshi5220@gmail.com>
2025-09-18 06:51:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -56,15 +56,4 @@ document.addEventListener('DOMContentLoaded', () => {
     searchInput.addEventListener('input', () => {
         filterProducts();
     });
-    const swiper = new Swiper('.swiper', {
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
 });
